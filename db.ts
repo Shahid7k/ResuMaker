f22111a8c87dd21996fc3d3183447894f0a1ec92
Shahid7k <shahid71733@gmail.com>
2020-07-15 23:03:34 +0530

    Deploy_trial_4

--- db.ts
+++ db.ts
@@ -3,7 +3,7 @@ import { MongoClient,ObjectId } from 'https://deno.land/x/mongo@v0.8.0/mod.ts';
 const NODE_API_URI :string= "mongodb+srv://Shahid:Professional7@deno-portfolio.4bf5q.mongodb.net/test";
 
 const mongoClient = new MongoClient();
-mongoClient.connectWithUri(NODE_API_URI)
+mongoClient.connectWithUri("mongodb+srv://Shahid:Professional7@deno-portfolio.4bf5q.mongodb.net/test")
 
 const db = mongoClient.database('users')
 
